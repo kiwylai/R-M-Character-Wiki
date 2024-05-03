a0ad92d8d6f01fc616b8dd0e9b8e46a90d5bdcac
Kiwy <kiwylai@gmial.com>
2024-05-03 11:14:04 +0200

    done

--- r-m-character-wiki/src/components/Cards/Cards.js
+++ r-m-character-wiki/src/components/Cards/Cards.js
@@ -1,20 +1,63 @@
 import React from "react";
+import styles from "./Cards.module.scss";
+import { Link } from "react-router-dom";
 
-const Cards = ({ results }) => {
+const Cards = ({ results, page }) => {
   let dispaly;
 
   if (results) {
     dispaly = results.map((x) => {
-      let { id, name, image } = x;
+      let { id, name, image, location, status } = x;
       return (
-        <div key={id} className="col-4">
-          <div className="">
-            <img src={image} alt="" className="img-fluid" />
-            <div className="content">
-              <div className="">{name}</div>
+        <Link
+          style={{ textDecoration: "none" }}
+          to={`${page}${id}`}
+          key={id}
+          className="col-lg-4 
+          col-md-6 col-12
+          mb-4 position-relative text-dark"
+        >
+          <div
+            className={`${styles.cards} d-flex flex-column justify-content-center`}
+          >
+            <img src={image} alt="" className={`img-fluid ${styles.img}`} />
+            <div style={{ padding: "10px" }} className="content">
+              <div className="fs-4 fw-bold mb-4">{name}</div>
+              <div className="">
+                <div className="">Last Location</div>
+                <div className="fs-5">{location.name}</div>
+              </div>
             </div>
           </div>
-        </div>
+
+          {(() => {
+            if (status === "Dead") {
+              return (
+                <div
+                  className={`position-absolute badge bg-danger ${styles.badge}`}
+                >
+                  {status}
+                </div>
+              );
+            } else if (status === "Alive") {
+              return (
+                <div
+                  className={`position-absolute badge bg-success ${styles.badge}`}
+                >
+                  {status}
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  className={`position-absolute badge bg-secondary ${styles.badge}`}
+                >
+                  {status}
+                </div>
+              );
+            }
+          })()}
+        </Link>
       );
     });
   } else {
